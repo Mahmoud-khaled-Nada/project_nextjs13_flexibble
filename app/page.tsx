9ffcfea255c9fbda7abdb4f51248d0c1401aed6b
Mahmoud-khaled-Nada <mahmoudkhalednada00@gmail.com>
2023-09-14 00:47:34 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = async () => {
+  return (
+    <section className="flexStart flex-col paddings">
+      <h1>Hello 1</h1>
+      <h1>Hello 2</h1>
+      <h1>Hello 3</h1>
+    </section>
+  );
+};
+
+export default Home;
